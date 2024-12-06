f07eae132bdd39baf776b1f88ed2ec599cd43b41
Cosimo UNGARO <cosimo.ungaro@telecomnancy.net>
2024-12-06 20:46:05 +0100

    Class definition

--- cli-wrapped.py
+++ cli-wrapped.py
@@ -3,3 +3,17 @@ import subprocess
 import pyfiglet
 from collections import Counter
 from pathlib import Path
+
+class CliWrapped:
+    
+    def __init__(self):
+        self.home_directory = os.path.expanduser("~")
+        self.commands = []
+        self.open_file()
+        self.parse_file()
+        self.print_results()
+
+
+
+if __name__=="__main__":
+    cliwrapped = CliWrapped()
